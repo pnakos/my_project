709f8e1bc543826da6cf2125e35a29292c02ef34
Pantelis Nakos <spamme2136@gmail.com>
2013-04-16 14:25:37 +0300

    argparse, fuzz_connection

--- dip_plain.py
+++ dip_plain.py
@@ -1,7 +1,15 @@
+#!/usr/bin/python
+
 import sleekxmpp
 import random
 import time
 import logging
+import argparse
+
+# Argument parser set-up
+parser = argparse.ArgumentParser()
+parser.add_argument("--connection", help="Fuzz the JID fields(node, domain, resource)", action="store_true")# action="store_true" no need for argument
+args = parser.parse_args()
 
 #Setup logging
 logging.basicConfig(format='%(levelname)-8s %(message)s')
@@ -13,9 +21,30 @@ def message(xmpp):
 	xmpp.send_message(mto='ken@ubuntu',mbody='Hello World!!!', 
 		mtype= 'chat')
 
+def fuzz_connection():
+	payload = 'a' * 1024
+
+	xmpp = sleekxmpp.ClientXMPP(payload +'@ubuntu/test', 'bill')
+	xmpp['feature_mechanisms'].unencrypted_plain = True # Force plaintext authentication https://groups.google.com/forum/?fromgroups=#!topic/sleekxmpp-discussion/RCzU4qa0Bfg
+	xmpp.connect(address=('172.16.206.152', 5222), use_tls = False)
+	xmpp.disconnect(wait=True)
+
+	# xmpp = sleekxmpp.ClientXMPP('bill@' + payload + '/test', 'bill')
+	# xmpp['feature_mechanisms'].unencrypted_plain = True # Force plaintext authentication https://groups.google.com/forum/?fromgroups=#!topic/sleekxmpp-discussion/RCzU4qa0Bfg
+	# xmpp.connect(address=('172.16.206.152', 5222), use_tls = False)
+	# xmpp.disconnect(wait=True)
+
+	xmpp = sleekxmpp.ClientXMPP('bill@ubuntu/' + payload, 'bill')
+	xmpp['feature_mechanisms'].unencrypted_plain = True # Force plaintext authentication https://groups.google.com/forum/?fromgroups=#!topic/sleekxmpp-discussion/RCzU4qa0Bfg
+	xmpp.connect(address=('172.16.206.152', 5222), use_tls = False)
+	xmpp.disconnect(wait=True)
+
+	xmpp = sleekxmpp.ClientXMPP(payload + '@ubuntu/' + payload, 'bill')
+	xmpp['feature_mechanisms'].unencrypted_plain = True # Force plaintext authentication https://groups.google.com/forum/?fromgroups=#!topic/sleekxmpp-discussion/RCzU4qa0Bfg
+	xmpp.connect(address=('172.16.206.152', 5222), use_tls = False)
+	xmpp.disconnect(wait=True)
 	
 def main():
-	name = 'a'*10240
 	xmpp = sleekxmpp.ClientXMPP('bill@ubuntu/test', 'bill')
 	xmpp['feature_mechanisms'].unencrypted_plain = True # Force plaintext authentication https://groups.google.com/forum/?fromgroups=#!topic/sleekxmpp-discussion/RCzU4qa0Bfg
 	xmpp.connect(address=('172.16.206.152', 5222), use_tls = False)
@@ -34,4 +63,8 @@ def main():
 
 
 if __name__ == '__main__':
-	main()
+	if args.connection:
+		print "Fuzzing JID fields..."
+		fuzz_connection()
+	else:
+		main()
