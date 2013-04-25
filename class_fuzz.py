7c415e65aa81f08c40ae98b02c84e72c5ca1a18d
Pantelis Nakos <spamme2136@gmail.com>
2013-04-25 13:56:58 +0300

    MessageFuzzer, removed logging...

--- class_fuzz.py
+++ class_fuzz.py
@@ -1,31 +1,53 @@
 #!/usr/bin/python
 
+"""
+	XMPP Fuzzer v0.1
+
+	An XMPP Fuzzer based on a tweaked SleekXMPP: The Sleek XMPP Library
+	Copyright (C) 2010  Nathanael C. Fritz
+
+	The fields that are fuzzed are the JID , message to: field and 
+	message type field.
+
+	pnakos@gmail.com
+"""
+
 import sleekxmpp
 import random
 import time
-import logging
+# import logging
 import argparse
-import thread
+# import thread
 import sys
 
 class MessageFuzzer(sleekxmpp.ClientXMPP):
-	"""docstring for Fuzz
 	"""
-	def __init__(self, jid, password):
+	docstring for Fuzz
+	"""
+	def __init__(self, jid, password, recipient, msg_typ):
 		sleekxmpp.ClientXMPP.__init__(self, jid, password)
 		self['feature_mechanisms'].unencrypted_plain = True # Force plaintext authentication https://groups.google.com/forum/?fromgroups=#!topic/sleekxmpp-discussion/RCzU4qa0Bfg
 		self.connect(address=('172.16.206.152', 5222), use_tls = False)
 		self.process(block=False)
+		self.recipient = recipient
+		self.msg_typ = msg_typ
 		self.add_event_handler("session_start", self.start)
 
 	def start(self, event):
+		"""
+		Process the session_start event.
+		"""
 		self.send_presence()
-		self.send_message(mto="ken@ubuntu",mbody="Hello World!!!",mtype='chat')
+		self.get_roster()
+
+		self.send_message(mto=self.recipient,mbody="Hello World!!!",mtype=self.msg_typ)
+
 		self.disconnect(wait=True)
 
 
 class ConnectionFuzzer(sleekxmpp.ClientXMPP):
-	"""docstring for ConnectionFuzzer
+	"""
+	docstring for ConnectionFuzzer
 	"""
 	def __init__(self, jid, password):
 		sleekxmpp.ClientXMPP.__init__(self, jid, password)
@@ -37,6 +59,9 @@ class ConnectionFuzzer(sleekxmpp.ClientXMPP):
 		# self.add_event_handler("disconnected", self.disconnected, threaded=True)
 
 	def start(self, event):
+		"""
+		Process the session_start event.
+		"""
 		self.send_presence()
 		self.disconnect(wait=False)
 		
@@ -57,7 +82,7 @@ if __name__ == '__main__':
 	args = parser.parse_args()
 
 	# Setup logging
-	logging.basicConfig(format='%(levelname)-8s %(message)s')
+	# logging.basicConfig(format='%(levelname)-8s %(message)s')
 
 	# Open fuzzing strings file
 	fuzz = open("fuzz_strings.txt", "r")
@@ -69,14 +94,26 @@ if __name__ == '__main__':
 		while line != '':
 			print "Fuzzing string... " + line.strip()
 			#thread.start_new_thread(ConnectionFuzzer,(line.strip() + '@ubuntu/test', 'bill'))
-			# ConnectionFuzzer(line.strip() + "@ubuntu/test", "bill")
-			ConnectionFuzzer("bill@" + line.strip() + "/test", "bill")
-			# ConnectionFuzzer("bill@ubuntu/" + line.strip(), "bill")
+			ConnectionFuzzer(line.strip() + "@ubuntu/test", "bill")
+			# ConnectionFuzzer("bill@" + line.strip() + "/test", "bill")
+			ConnectionFuzzer("bill@ubuntu/" + line.strip(), "bill")
 			time.sleep(0.25)
 			line = fuzz.readline()
 
 		fuzz.close()
-		sys.exit(1)
+		sys.exit()
 				
 	else:
-		MessageFuzzer("bill@ubuntu/test", "bill")
+		
+		print "Fuzzing message fields mto and mtype fields..."
+		line = fuzz.readline()
+		
+		while line != '':
+			print "Fuzzing string... " + line.strip()
+			MessageFuzzer("bill@ubuntu/test", "bill", line + "@ubuntu", line)
+			time.sleep(0.25)
+			line = fuzz.readline()
+
+		fuzz.close()
+		sys.exit()
+		
