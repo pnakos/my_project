d6102cac7abca97279c332e5c47c05685f8c4d7f
Pantelis Nakos <spamme2136@gmail.com>
2013-11-05 13:22:04 +0200

    Try-except added

--- xmpp_fuzzer.py
+++ xmpp_fuzzer.py
@@ -1,6 +1,15 @@
 #!/usr/bin/python
 
-"""
+import sleekxmpp
+# import random
+import time
+import re
+import argparse
+import sys
+import logging
+
+def banner():
+	"""
 	XMPP Fuzzer v0.1
 
 	An XMPP Fuzzer based on a tweaked SleekXMPP: The Sleek XMPP Library
@@ -12,26 +21,22 @@
 	Pantelis Nakos, University of Piraeus, 2013
 
 	pnakos@gmail.com
-"""
 
-import sleekxmpp
-import random
-import time
-import re
-import argparse
-import sys
-import logging
+	"""
+	return banner.__doc__
 
-logging.basicConfig(level=logging.ERROR) #levels CRITICAL, ERROR, WARNING, INFO or DEBUG
+print banner()
+
+#logging.basicConfig(level=logging.ERROR) #levels CRITICAL, ERROR, WARNING, INFO or DEBUG
 
 # Regular expression for a valid JID format
 JID_PATTERN = re.compile("^(?:(.{1,10250})@)?([^/@]{1,10250})(?:/(.{1,10250}))?$")
 
 class MessageFuzzer(sleekxmpp.ClientXMPP):
 	"""
-		A basic sleekXMPP bot that will log in, 
-		send a message and then log out.
-	
+	A basic sleekXMPP bot that will log in, 
+	send a message and then log out.
+
 	"""
 	def __init__(self, jid, password, recipient, subject, msg_typ):
 		sleekxmpp.ClientXMPP.__init__(self, jid, password)
@@ -45,7 +50,8 @@ class MessageFuzzer(sleekxmpp.ClientXMPP):
 
 	def start(self, event):
 		"""
-			Process the session_start event.
+		Process the session_start event.
+
 		"""
 		self.send_presence()
 		self.get_roster()
@@ -57,8 +63,9 @@ class MessageFuzzer(sleekxmpp.ClientXMPP):
 
 class ConnectionFuzzer(sleekxmpp.ClientXMPP):
 	"""
-		A basic sleekXMPP bot that will try to log in
-		and then log out.
+	A basic sleekXMPP bot that will try to log in
+	and then log out.
+
 	"""
 	def __init__(self, jid, password):
 		sleekxmpp.ClientXMPP.__init__(self, jid, password)
@@ -69,78 +76,83 @@ class ConnectionFuzzer(sleekxmpp.ClientXMPP):
 		
 	def start(self, event):
 		"""
-			Process the session_start event.
+		Process the session_start event.
+
 		"""
 		self.send_presence()
 		self.disconnect(wait=False)
 	
 
 if __name__ == '__main__':
+	try:
+		# Argument parser set-up
+		parser = argparse.ArgumentParser()
+		parser.add_argument("-s", "--server", help="XMPP server IP address to fuzz")
+		parser.add_argument("-l", "--login", help="Valid login name for message fuzzing(e.g username@domain)")
+		parser.add_argument("-p", "--password", help="Password for the login name provided")
+		parser.add_argument("-c", "--connection", help="Fuzz the JID fields(node, domain, resource)", action="store_true")# action="store_true" not mandatory argument
+		args = parser.parse_args()
 
-	# Argument parser set-up
-	parser = argparse.ArgumentParser()
-	parser.add_argument("-s", "--server", help="XMPP server address to fuzz")
-	parser.add_argument("-l", "--login", help="Valid login name (e.g username@domain)")
-	parser.add_argument("-p", "--password", help="Password for the login name provided")
-	parser.add_argument("-c","--connection", help="Fuzz the JID fields(node, domain, resource)", action="store_true")# action="store_true" not mandatory argument
-	args = parser.parse_args()
-
-	def parse_JID(data):
-		"""
-			Parse the username and domain 
+		def parse_JID(data):
+			"""
+			Parse the username and domain name
 			from the JID.
-		"""
 
-		match = JID_PATTERN.match(data)
+			"""
 
-		(node, domain, resource) = match.groups()
+			match = JID_PATTERN.match(data)
 
-		return node, domain
+			(node, domain, resource) = match.groups()
 
-	jid = parse_JID(args.login)
+			return node, domain
 
-	usrname = jid[0]
+		jid = parse_JID(args.login)
 
-	domain = jid[1]
+		usrname = jid[0]
 
-	# Open fuzzing strings file
-	fuzz = open("fuzz_strings.txt", "r")
+		domain = jid[1]
 
-	if args.connection:
-		print "Fuzzing JID fields..."
-		line = fuzz.readline()
-		flag = 0
-		
-		# For every line in file call ConnectionFuzzer using the line for username
-		# and /resource.
-		while line != '':
-			print str(flag) + " Fuzzing string... " + line.strip()
-			ConnectionFuzzer(line.strip() + "@" + line.strip() + "/" + line.strip(), "pass")
-			ConnectionFuzzer(line.strip() + "@" + domain + "/test", args.password)
-			ConnectionFuzzer(usrname + "@" + line.strip() + "/test", args.password)
-			ConnectionFuzzer(args.login + "/" + line.strip(), args.password)
-			time.sleep(0.25)
+		# Open fuzzing strings file
+		fuzz = open("fuzz_strings.txt", "r")
+
+		if args.connection:
+			print "Fuzzing JID fields..."
 			line = fuzz.readline()
-			flag += 1
+			flag = 0
+			
+			# For every line in file call ConnectionFuzzer using the line for username
+			# and /resource.
+			while line != '':
+				print str(flag) + " Fuzzing string... " + line.strip()
+				ConnectionFuzzer(line.strip() + "@" + line.strip() + "/" + line.strip(), "pass")
+				ConnectionFuzzer(line.strip() + "@" + domain + "/test", args.password)
+				ConnectionFuzzer(usrname + "@" + line.strip() + "/test", args.password)
+				ConnectionFuzzer(args.login + "/" + line.strip(), args.password)
+				time.sleep(0.25)
+				line = fuzz.readline()
+				flag += 1
 
-		fuzz.close()
-		sys.exit()
-				
-	else:
-		
-		print "Fuzzing message, mto and mtype fields..."
-		line = fuzz.readline()
-		flag1 = 0
-		
-		# For every line in file call MessageFuzzer using the line for recipients
-		# name and message type.
-		while line != '':
-			print str(flag1) + " Fuzzing string... " + line.strip()
-			MessageFuzzer(args.login, args.password, line.strip() + "@" + domain, line.strip(), line.strip())
-			time.sleep(0.25)
+			fuzz.close()
+			sys.exit()
+					
+		else:
+			
+			print "Fuzzing message fields mto, msubject and mtype..."
 			line = fuzz.readline()
-			flag1 += 1
+			flag1 = 0
+			
+			# For every line in file call MessageFuzzer using the line for recipients
+			# name and message type.
+			while line != '':
+				print str(flag1) + " Fuzzing string... " + line.strip()
+				MessageFuzzer(args.login, args.password, line.strip() + "@" + domain, line.strip(), line.strip())
+				time.sleep(0.25)
+				line = fuzz.readline()
+				flag1 += 1
 
-		fuzz.close()
+			fuzz.close()
+			sys.exit()
+	except TypeError:
+		print "Wrong or no arguments given. Use -h for help."
 		sys.exit()
 		
