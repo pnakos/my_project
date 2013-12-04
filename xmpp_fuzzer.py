8ba3eeef7efbdf9e9a6ac81644eaa215a0492012
Pantelis Nakos <spamme2136@gmail.com>
2013-12-04 13:51:22 +0200

    Final...

--- xmpp_fuzzer.py
+++ xmpp_fuzzer.py
@@ -11,8 +11,9 @@ def banner():
 	"""
 	XMPP Fuzzer v0.1
 
-	An XMPP Fuzzer based on a tweaked SleekXMPP: The Sleek XMPP Library
-	Copyright (C) 2010  Nathanael C. Fritz
+	An XMPP Fuzzer based on a modified MIT Licensed 
+	SleekXMPP: The Sleek XMPP Library
+	Copyright (C) 2010  Nathanael C. Fritz, 
 
 	The fields that are fuzzed are JID , "message to:" field, "subject" 
 	field and "message type" field.
